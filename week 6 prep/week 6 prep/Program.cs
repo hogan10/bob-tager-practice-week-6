47eefa4e1a9648c072f72537472bfb473ab2e4a6
Erin Hogan <hogan10@charter.net>
2018-08-14 23:08:26 -0500

    with notes

---
+++ week 6 prep/week 6 prep/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace week_6_prep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string myString = "My \"so-called\" life";
+            //string myString = "What if I need a\nnew line?";
+            //string myString = "go to your c:\\drive ";
+            //string myString = @" go to your c:\drive";
+            //string myString = String.Format("{0} = {1}", "First", "Second");
+            //string myString = string.Format("{0:C}", 123.45);
+            //string myString = string.Format("{0:N}", 1234567890);
+            //string myString = string.Format("{0:P}", .123);
+            //string myString = string.Format("Phone Number: {0:(###) ###-####}", 1234567890);
+            //string myString = " That summer we took threes across the board ";
+            //myString = myString.Substring(6, 14);
+            //myString = myString.ToUpper();
+            //myString = myString.Replace(" ", "--");
+            //myString = myString.Remove(6, 14);
+            /*
+            myString = string.Format("Length before: {0} -- Lengthe after: {1}",
+                myString.Length,
+                myString.Trim().Length);
+                */
+
+            /*
+            string myString = " ";
+            for (int i = 0; i < 100; i++)
+            {
+                myString += "--" + i.ToString();
+            }
+            */
+
+            StringBuilder myString = new StringBuilder();
+
+            for (int i = 0; i < 100; i++)
+            {
+                myString.Append("--");
+                myString.Append(i);
+            }
+              Console.WriteLine(myString);
+            Console.ReadLine();
+        }
+    }
+}
